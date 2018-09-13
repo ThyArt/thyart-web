41d7ba84f3c7b2c6669624b32206534c0e1f79df
thoma_m <maxime.thomas@epitech.eu>
2018-09-13 22:12:42 +0200

    first version

--- src/Account.js
+++ src/Account.js
@@ -1,12 +1,84 @@
 import React, {Component} from "react";
 
+import './Account.css'
+import {Nav, Navbar, NavItem, } from "react-bootstrap";
+
+
 class Account extends Component {
   render() {
     return (
         <div>
-          <h2>You are connected</h2>
-          <h3>Token:</h3>
-          <p>{this.props.location.state.token}</p>
+            <div>
+                <Navbar id="navBarAccount" fixedTop>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a>
+                                <img src={require('./static/SmallLogo.png')} alt="logo" height="100" width="auto" id="logo"/>
+                            </a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle/>
+                    </Navbar.Header>
+                </Navbar>
+            </div>
+
+
+
+            <div >
+                <nav className="col-md-2">
+                    <Navbar id="sideBar">
+                        <div className="sidebar-sticky">
+                            <ul className="nav nav flex-column">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#" id="nav-link">
+                                        Dashboard
+                                        <span className="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#"  id="nav-link">
+                                        Orders
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#" id="nav-link">
+                                        Factu
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#" id="nav-link">
+                                        Stats
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#" id="nav-link">
+                                        Oeuvre
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#" id="nav-link">
+                                        Client
+                                    </a>
+                                </li>
+                            </ul>
+                            <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                                <span id="nav-link">_____________</span>
+                            </h6>
+                            <ul className="nav flex-column mb-2">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#" id="nav-link">
+                                        profil
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#" id="nav-link">
+                                        deco
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </Navbar>
+                </nav>
+            </div>
         </div>
     );
   }
