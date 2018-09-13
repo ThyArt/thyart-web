213a185b800e1e9762dbe79b860e7a2c886659de
thoma_m <maxime.thomas@epitech.eu>
2018-09-13 23:05:39 +0200

    Complete AccountPage
    Change menu with <ul> by <NavItem> and <NavText>
    Change the sideNav color, select and align
    Add a Title

--- src/Account.js
+++ src/Account.js
@@ -1,8 +1,12 @@
 import React, {Component} from "react";
 
 import './Account.css'
-import {Nav, Navbar, NavItem, } from "react-bootstrap";
+import {Navbar} from "react-bootstrap";
 
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 class Account extends Component {
   render() {
@@ -18,67 +22,60 @@ class Account extends Component {
                         </Navbar.Brand>
                         <Navbar.Toggle/>
                     </Navbar.Header>
+                        <NavItem>
+                        <NavText>
+                            <h1 style={{color: "white", textAlign :"center"}}>
+                                Ma gallerie
+                            </h1>
+                        </NavText>
+                        </NavItem>
+
                 </Navbar>
             </div>
 
 
-
-            <div >
-                <nav className="col-md-2">
-                    <Navbar id="sideBar">
-                        <div className="sidebar-sticky">
-                            <ul className="nav nav flex-column">
-                                <li className="nav-item">
-                                    <a className="nav-link active" href="#" id="nav-link">
-                                        Dashboard
-                                        <span className="sr-only">(current)</span>
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#"  id="nav-link">
-                                        Orders
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#" id="nav-link">
-                                        Factu
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#" id="nav-link">
-                                        Stats
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#" id="nav-link">
-                                        Oeuvre
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#" id="nav-link">
-                                        Client
-                                    </a>
-                                </li>
-                            </ul>
-                            <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                                <span id="nav-link">_____________</span>
-                            </h6>
-                            <ul className="nav flex-column mb-2">
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#" id="nav-link">
-                                        profil
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#" id="nav-link">
-                                        deco
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </Navbar>
-                </nav>
-            </div>
+            <SideNav id="sideNav" >
+                <SideNav.Nav defaultSelected="home">
+                    <NavItem eventKey="home">
+                        <NavText id="navText">
+                            Acceuil
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="statistiques">
+                        <NavText id="navText">
+                            Statistiques
+                        </NavText>
+                    </NavItem>
+                    <NavItem id="navItem" eventKey="oeuvres">
+                        <NavText id="navText">
+                            Oeuvres
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="Clients">
+                        <NavText id="navText">
+                            Clients
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="Facturation">
+                        <NavText id="navText">
+                            Facturation
+                        </NavText>
+                    </NavItem>
+                    <h6 id="splitSideBar">
+                        <span>______________</span>
+                    </h6>
+                    <NavItem eventKey="Facturation">
+                        <NavText id="navText">
+                            Profil
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="Deconnection">
+                        <NavText id="navText">
+                            Déconnection
+                        </NavText>
+                    </NavItem>
+                </SideNav.Nav>
+            </SideNav>
         </div>
     );
   }
