d556888cecfea5dfa7a5af479e600cbd09bf472c
thoma_m <maxime.thomas@epitech.eu>
2019-03-24 14:20:29 -0700

    Creation of client section

--- src/containers/Account.jsx
+++ src/containers/Account.jsx
@@ -4,6 +4,7 @@ import Scheduler from '../components/account/Scheduler';
 import Profile from '../components/account/Profile';
 import Members from '../components/account/Members';
 import Artwork from '../components/account/Artwork';
+import Client from '../components/account/Client';
 import {Redirect} from 'react-router-dom';
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
@@ -66,6 +67,9 @@ export class Account extends Component {
               <NavItem  eventKey={4} className='item'>
                 Membres
               </NavItem>
+              <NavItem  eventKey={8} className='item'>
+                Client
+              </NavItem>
               <NavItem  eventKey={5} className='item'>
                 Facturation
               </NavItem>
@@ -83,6 +87,7 @@ export class Account extends Component {
             { this.state.selected === 4 ? <Members token={this.state.token}/> : null}
             { this.state.selected === 3 ? <Artwork token={this.state.token}/> : null }
             { this.state.selected === 6 ? <Profile token={this.state.token}/> : null }
+            { this.state.selected === 8 ? <Client token={this.state.token}/> : null }
           </div>
 
 
