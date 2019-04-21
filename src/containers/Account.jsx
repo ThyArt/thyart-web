45cfd081bb28a484a670954f7990490a28941db1
thoma_m <maxime.thomas@epitech.eu>
2019-04-21 10:51:42 -0700

    Creation page facturation

--- src/containers/Account.jsx
+++ src/containers/Account.jsx
@@ -5,6 +5,7 @@ import Profile from '../components/account/Profile';
 import Members from '../components/account/Members';
 import Artwork from '../components/account/Artwork';
 import Client from '../components/account/Client';
+import Facturation from '../components/account/Facturation';
 import {Redirect} from 'react-router-dom';
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
@@ -86,6 +87,7 @@ class Account extends Component {
             { this.state.selected === 3 ? <Artwork token={this.state.token}/> : null }
             { this.state.selected === 6 ? <Profile token={this.state.token}/> : null }
             { this.state.selected === 8 ? <Client token={this.state.token}/> : null }
+            { this.state.selected === 5 ? <Facturation token={this.state.token}/> : null }
           </div>
 
 
