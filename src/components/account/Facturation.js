3900620a5e8300bc33f19a724a74bae6b8f845fc
Maxence Tisserant <maxence.tisserant@epitech.eu>
2019-04-22 10:43:24 +0300

    Beginning of billing component

--- src/components/account/Facturation.js
+++ src/components/account/Facturation.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import { cloneDeep, findIndex } from 'lodash';
 import * as Table from 'reactabular-table';
-import uuid from 'uuid';
-import {Button, Col, FormControl, FormGroup} from "react-bootstrap";
+import {Button, Col} from "react-bootstrap";
 import Modal from "react-responsive-modal";
 import {Redirect} from 'react-router-dom';
 
@@ -15,15 +14,10 @@ class Client extends Component {
         this.state = {
             rows: [], // initial rows
             columns: this.getColumns(), // initial columns
-            addModal: false,
-            detailsModal: false,
             removeModal: false,
             clientRedirect: false,
-            newName: '',
-            newFamily: '',
-            newMail: '',
-            newAddress:'',
-            newTelephone:'',
+            addDetailsRedirect: false,
+            infos: {},
             currentClient: []
         };
 
@@ -33,87 +27,11 @@ class Client extends Component {
     }
 
     onAddOpen = () => {
-        this.setState({ addModal: true });
-    };
-
-    onAddClose = () => {
-        this.setState({ newName: '', addModal: false });
+        this.setState({ infos: {}, addDetailsRedirect: true });
     };
 
     onRemoveClose = () => {
         this.setState({ newName: '', removeModal: false });
-    };
-
-    onDetailsClose = () => {
-        this.setState({ detailsModal: false });
-    };
-
-    handleChangeMail = event => {
-        this.setState({ newMail: event.target.value });
-    };
-
-    handleChangeAddress = event => {
-        this.setState({ newAddress: event.target.value });
-    };
-
-    handleChangeTelephone = event => {
-        this.setState({ newTelephone: event.target.value });
-    };
-
-    handleChangeName = event => {
-        this.setState({ newName: event.target.value });
-    };
-
-    handleChangeFamily = event => {
-        this.setState({ newFamily: event.target.value });
-    };
-
-    getNameValidationState() {
-        let name = this.state.newName;
-        if (name === '') return 'error';
-        return 'success';
-    };
-
-    getFamilyValidationState() {
-        let family = this.state.newFamily;
-        if (family === '') return 'error';
-        return 'success';
-    };
-
-    getMailValidationState() {
-        let email = this.state.newMail;
-        if (email === '') return 'error';
-        let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-        if (re.test(email)) {
-            return 'success';
-        } else {
-            return 'error';
-        }
-    };
-
-    getAddressValidationState() {
-        let address = this.state.newAddress;
-        if (address === '') return 'error';
-        let re = /^.+$/;
-
-        if (re.test(address)) {
-            return 'success';
-        } else {
-            return 'error';
-        }
-    };
-
-    getNumberValidationState() {
-        let number = this.state.newTelephone;
-        if (number === '') return 'error';
-        let re = /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/;
-
-        if (re.test(number)) {
-            return 'success';
-        } else {
-            return 'error';
-        }
     };
 
     getColumns() {
@@ -191,6 +109,8 @@ class Client extends Component {
 
                 <tbody>
 
+                { this.state.addDetailsRedirect ? <Redirect to={{ pathname: "/billing", state: {...this.state.infos} }}/> : null }
+
                 <Col sm={10}>
                     {this.props.add}
                     <button className='add' onClick={this.onAddOpen}>
@@ -198,101 +118,6 @@ class Client extends Component {
                         <span className='add'>Ajouter</span>
                     </button>
                 </Col>
-
-
-                <Modal open={this.state.addModal} onClose={this.onAddClose} center>
-                    <h2 className='title'>Ajout d'une facturation :</h2>
-                    <Col sm={6}>
-                        <h3 className="firstname">Client :</h3>
-                    </Col>
-                    <Col sm={6}>
-                        <FormGroup className='input' validationState={this.getFamilyValidationState()}>
-                            <FormControl
-                                type="firstname"
-                                value={this.props.newFamily}
-                                placeholder="Entrer le nom du client"
-                                onChange={this.handleChangeFamily}
-                            />
-                            <FormControl.Feedback />
-                        </FormGroup>
-                    </Col>
-                    <Col sm={6}>
-                        <h3 className="name">Oeuvre :</h3>
-                    </Col>
-                    <Col sm={6}>
-                        <FormGroup className='input' validationState={this.getNameValidationState()}>
-                            <FormControl
-                                type="name"
-                                value={this.props.newName}
-                                placeholder="Entrer le nom de l'oeuvre"
-                                onChange={this.handleChangeName}
-                            />
-                            <FormControl.Feedback />
-                        </FormGroup>
-                    </Col>
-                    <Col sm={6}>
-                        <h3 className="name">Date :</h3>
-                    </Col>
-                    <Col sm={6}>
-                        <FormGroup className='input' validationState={this.getMailValidationState()}>
-                            <FormControl
-                                type="mail"
-                                value={this.props.newMail}
-                                placeholder="Entrer la date du jour"
-                                onChange={this.handleChangeMail}
-                            />
-                            <FormControl.Feedback />
-                        </FormGroup>
-                    </Col>
-                        <Col sm={6}>
-                        <h3 className="name">Adresse :</h3>
-                    </Col>
-                    <Col sm={6}>
-                        <FormGroup className='input' validationState={this.getAddressValidationState()}>
-                            <FormControl
-                                type="adresse"
-                                value={this.props.newAddress}
-                                placeholder="Entrer l'adresse du client"
-                                onChange={this.handleChangeAddress}
-                            />
-                            <FormControl.Feedback />
-                        </FormGroup>
-                    </Col>
-                    <Col sm={6}>
-                        <h3 className="name">Téléphone :</h3>
-                    </Col>
-                    <Col sm={6}>
-                        <FormGroup className='input' validationState={this.getNumberValidationState()}>
-                            <FormControl
-                                type="Phone number"
-                                value={this.props.newTelephone}
-                                placeholder="Entrer le numéro du client"
-                                onChange={this.handleChangeTelephone}
-                            />
-                            <FormControl.Feedback />
-                        </FormGroup>
-                    </Col>
-                    <Button bsStyle="primary" onClick={this.onAdd} className='validate' bsSize='large'>
-                        Valider
-                    </Button>
-                </Modal>
-
-                <Modal open={this.state.detailsModal} onClose={this.onDetailsClose} center>
-                    <h2 className='title'>Détails du client</h2>
-                    <Col sm={6} className="name">Nom :</Col>
-                    <Col sm={6} className="name">{this.state.currentClient['family']}</Col>
-                    <Col sm={6} className="name">Prénom :</Col>
-                    <Col sm={6} className="name">{this.state.currentClient['name']}</Col>
-                    <Col sm={6} className="name">Mail :</Col>
-                    <Col sm={6} className="name">{this.state.currentClient['mail']}</Col>
-                    <Col sm={6} className="name">Adresse :</Col>
-                    <Col sm={6} className="name">{this.state.currentClient['address']}</Col>
-                    <Col sm={6} className="name">Téléphone :</Col>
-                    <Col sm={6} className="name">{this.state.currentClient['number']}</Col>
-                    <Button bsStyle="primary" className='validate' bsSize='large' onClick={this.openInTab}>
-                        Plus de détails
-                    </Button>
-                </Modal>
 
                 <Modal open={this.state.removeModal} onClose={this.onRemoveClose} center>
                     <h2 className='title'>Voulez-vous supprimer ce client?</h2>
@@ -316,31 +141,11 @@ class Client extends Component {
     }
 
     onAdd(e) {
-        if (this.getNameValidationState() === 'success' && this.getMailValidationState() === 'success'
-            && this.getFamilyValidationState() === 'success' && this.getNumberValidationState() === 'success') {
-            e.preventDefault();
-
-            const rows = cloneDeep(this.state.rows);
-
-            rows.unshift({
-                id: uuid.v4(),
-                name: this.state.newName,
-                family: this.state.newFamily,
-                mail: this.state.newMail,
-                address: this.state.newAddress,
-                number: this.state.newTelephone
-            });
-
-            this.setState({ rows, addModal: false, newName: '', newFamily: '', newMail: '', newAddress:'', newTelephone:'' });
-        }
+    //    TODO: Add row once billing is saved
     };
 
     openDetails(rowData) {
-        this.setState({ detailsModal: true, currentClient: rowData });
-    };
-
-    openInTab = () => {
-        this.setState({ clientRedirect: true });
+        this.setState({ infos: rowData, addDetailsRedirect: true });
     };
 
     confirmRemove(id) {
