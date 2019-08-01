adb61cec933c2c62125eda78061565a62832e760
Clément Deseine <clement.deseine@gmail.com>
2019-08-01 21:24:13 +0200

    fix folder to deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,12 +1,12 @@
 if [[ "$TRAVIS_BRANCH" == 'feature/travis-integration' ]];
 then
   yarn build
-  rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/build root@staging.thyart.fr:/var/www/thyart/
+  rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/build/* root@staging.thyart.fr:/var/www/thyart/
   ssh root@staging.thyart.fr chown -R www-data:www-data /var/www/thyart/
 
 elif [[ "$TRAVIS_BRANCH" == 'master' ]];
 then
   yarn build
-  rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/build root@www.thyart.fr:/var/www/thyart/
+  rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/build/* root@www.thyart.fr:/var/www/thyart/
   ssh root@www.thyart.fr chown -R www-data:www-data /var/www/thyart/
 fi
