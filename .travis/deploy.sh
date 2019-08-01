44ac0b006972a7b888e2d3edc595defdb3fcbd9c
Clément Deseine <clement.deseine@gmail.com>
2019-08-01 20:58:39 +0200

    fix rights

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,11 +2,11 @@ if [[ "$TRAVIS_BRANCH" == 'feature/travis-integration' ]];
 then
   yarn build
   rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/build root@staging.thyart.fr:/var/www/thyart/
-  ssh root@staging.api.thyart.fr chown -R www-data:www-data /var/www/thyart-api/
+  ssh root@staging.thyart.fr chown -R www-data:www-data /var/www/thyart/
 
 elif [[ "$TRAVIS_BRANCH" == 'master' ]];
 then
   yarn build
   rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/build root@www.thyart.fr:/var/www/thyart/
-  ssh root@api.thyart.fr chown -R www-data:www-data /var/www/thyart-api/
+  ssh root@www.thyart.fr chown -R www-data:www-data /var/www/thyart/
 fi
