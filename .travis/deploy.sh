c3f7f1b5dec92fa7f5271e67ec6add8d625cbe5d
Clément Deseine <clement.deseine@gmail.com>
2019-08-01 20:27:31 +0200

    add deploy script

---
+++ .travis/deploy.sh
@@ -0,0 +1,10 @@
+if [[ "$TRAVIS_BRANCH" == 'feature/travis-integration' ]];
+then
+  rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/build root@staging.thyart.fr:/var/www/thyart/
+  ssh root@staging.api.thyart.fr chown -R www-data:www-data /var/www/thyart-api/
+
+elif [[ "$TRAVIS_BRANCH" == 'master' ]];
+then
+  rsync -r --delete-after --quiet "$TRAVIS_BUILD_DIR"/build root@www.thyart.fr:/var/www/thyart/
+  ssh root@api.thyart.fr chown -R www-data:www-data /var/www/thyart-api/
+fi
