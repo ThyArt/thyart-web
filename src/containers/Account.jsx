45a4a06f3443462e8c6f1426cd57fb78750a1e0b
Jean gabriel <jeangabri95@gmail.com>
2019-10-02 22:45:37 +0200

    new sidebar / upgrade dep

--- src/containers/Account.jsx
+++ src/containers/Account.jsx
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, Container, Row } from 'react-bootstrap';
-import Scheduler from '../components/account/Scheduler';
-import Profile from '../components/account/Profile';
-import Members from '../components/account/Members';
-import Artwork from '../components/account/Artwork';
-import Clients from '../components/account/Clients';
-import Billings from '../components/account/Billings';
-import Newsletter from '../components/account/Newsletters';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { disconnect } from '../actions/actionsAuth';
 
 import '../css/Account.css';
-import Col from 'react-bootstrap/Col';
 import {
   NotificationContainer,
   NotificationManager
 } from 'react-notifications';
-import Stats from "../components/account/Stats";
+import ResponsiveDrawer from './SideBar';
 
 export class Account extends Component {
   constructor(props) {
@@ -36,14 +28,9 @@ export class Account extends Component {
     if (token === null) this.props.dispatch(disconnect());
   }
 
-  handleSelect = eventKey => {
-    if (eventKey === '7') {
-      this.props.dispatch(disconnect());
-      this.setState({ token: null });
-      sessionStorage.removeItem('token');
-    } else {
-      this.setState({ selected: eventKey });
-    }
+  callbackFunction = (childData) => {
+    console.log(childData);
+    this.setState({token: childData})
   };
 
   render() {
@@ -65,79 +52,18 @@ export class Account extends Component {
           </Container>
         </Navbar>
         <Container fluid={true}>
-          <Row>
-            <Col>
-              <div>
-                <Nav
-                  className="flex-column"
-                  variant={'pills'}
-                  defaultActiveKey={'1'}
-                  onSelect={this.handleSelect}
-                >
-                  <Nav.Link eventKey={'1'} className="item">
-                    Accueil
-                  </Nav.Link>
-                  <Nav.Link eventKey={'2'} className="item">
-                    Statistiques
-                  </Nav.Link>
-                  <Nav.Link eventKey={'3'} className="item">
-                    Oeuvres
-                  </Nav.Link>
-                  <Nav.Link eventKey={'4'} className="item">
-                    Membres
-                  </Nav.Link>
-                  <Nav.Link eventKey={'8'} className="item">
-                    Client
-                  </Nav.Link>
-                  <Nav.Link eventKey={'5'} className="item">
-                    Facturation
-                  </Nav.Link>
-                  <Nav.Link eventKey={'6'} className="item">
-                    Profil
-                  </Nav.Link>
-                  <Nav.Link eventKey={'9'} className="item">
-                    Newsletter
-                  </Nav.Link>
-                  <Nav.Link eventKey={'7'}>Déconnexion</Nav.Link>
-                </Nav>
-              </div>
-            </Col>
-            <Col xs={10}>
-              <div>
-                {this.state.selected === '1' ? (
-                  <Scheduler token={this.state.token} />
-                ) : null}
-                {this.state.selected === '2' ? (
-                    <Stats token={this.state.token} />
-                ) : null}
-                {this.state.selected === '3' ? (
-                    <Artwork token={this.state.token} />
-                ) : null}
-                {this.state.selected === '4' ? (
-                  <Members token={this.state.token} />
-                ) : null}
-                {this.state.selected === '5' ? (
-                    <Billings token={this.state.token} />
-                ) : null}
-                {this.state.selected === '6' ? (
-                  <Profile token={this.state.token} />
-                ) : null}
-                {this.state.selected === '8' ? (
-                  <Clients token={this.state.token} />
-                ) : null}
-                {this.state.selected === '9' ? (
-                    <Newsletter token={this.state.token} />
-                ) : null}
-              </div>
-            </Col>
-          </Row>
+              <ResponsiveDrawer token={this.state.token}  parentCallback = {this.callbackFunction}>
+
+              </ResponsiveDrawer>
+
         </Container>
+
         {this.state.token === null ? (
-          <Redirect
-            to={{
-              pathname: '/signin'
-            }}
-          />
+            <Redirect
+                to={{
+                  pathname: '/signin'
+                }}
+            />
         ) : null}
       </div>
     );
