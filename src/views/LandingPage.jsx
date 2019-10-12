74f2b49fa1b695eaf6739635123bbeaaf64fbbc3
maxime <maxime.thomas@epitech.eu>
2019-10-12 15:29:44 +0200

    Preparation Profile

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CarouselPage from 'components/Carousel/CarouselPage';
+import Profile from 'views/Profil'
 
 export default function LandingPage() {
   return (
     <div>
       <CarouselPage />
+      <Profile/>
     </div>
   );
 }
