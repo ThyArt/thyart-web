8521ae4fba21422cc84725c25788194333ca4ed3
maxime <maxime.thomas@epitech.eu>
2019-10-12 15:42:45 +0200

    Fix error commit

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import CarouselPage from 'components/Carousel/CarouselPage';
-import Profile from 'views/Profil'
 
 export default function LandingPage() {
   return (
     <div>
       <CarouselPage />
-      <Profile/>
     </div>
   );
 }
