969511bb8d87f3ab161307a69aac2818dc86afb9
Clément Deseine <clement.deseine@gmail.com>
2019-10-13 20:26:03 +0200

    use lodash with About

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CarouselPage from 'components/Carousel/CarouselPage';
+import About from './About';
 
 export default function LandingPage() {
   return (
     <div>
       <CarouselPage />
+      <About />
     </div>
   );
 }
