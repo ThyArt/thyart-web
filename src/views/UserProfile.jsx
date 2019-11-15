a2742bfc875bf02063f81a0ffebedb1e78fecbae
Clément <clement.deseine@gmail.com>
2019-11-15 17:11:13 +0100

    use map in user profile

--- src/views/UserProfile.jsx
+++ src/views/UserProfile.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { map } from 'lodash';
 import EditIcon from '@material-ui/icons/Edit';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -10,7 +11,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { inheritInnerComments } from '@babel/types';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -44,225 +44,102 @@ const useStyles = makeStyles(theme => ({
 
 export default function Profile() {
   const classes = useStyles();
-  const [openFirstname, setOpenFirstname] = React.useState(false);
-  const [openName, setOpenName] = React.useState(false);
-  const [openMail, setOpenMail] = React.useState(false);
-  const [openPwd, setOpenPwd] = React.useState(false);
+  const [
+    {
+      open: modalOpen,
+      title: modalTitle,
+      stateName: modalStateName,
+      dialogText: modalDialogText,
+      textType: modalTextType
+    },
+    setModal
+  ] = useState({
+    open: false,
+    title: undefined,
+    stateName: undefined,
+    dialogText: undefined,
+    textType: undefined
+  });
 
-  /*const handleClickOpen = () => {
-    setOpen(true);
-  };
+  const closeModal = () =>
+    setModal({
+      open: false,
+      title: undefined,
+      stateName: undefined,
+      dialogText: undefined,
+      textType: undefined
+    });
 
-  const handleClose = () => {
-    setOpen(false);
-  };*/
+  const fields = [
+    {
+      title: 'Prénom',
+      stateName: 'firstname',
+      dialogText: 'Veuillez entrer votre prénom',
+      textType: 'text'
+    },
+    {
+      title: 'Nom',
+      stateName: 'lastname',
+      dialogText: "Veuillez entrer votre nom d'usage",
+      textType: 'text'
+    },
+    {
+      title: 'Email',
+      stateName: 'email',
+      dialogText: 'Veuillez entrer votre email',
+      textType: 'email'
+    },
+    {
+      title: 'Mot de Passe',
+      stateName: 'password',
+      dialogText: 'Veuillez entrer votre mot de passe',
+      textType: 'password'
+    }
+  ];
 
   return (
-    <div>
+    <Fragment>
       <h1> Vos informations : </h1>
-      <Grid container spacing={3}>
-        <Grid item md={3} sm={5} xs={8}>
-          <Paper className={classes.paper}>Prenom</Paper>
+      {map(fields, field => (
+        <Grid container spacing={3} key={field.stateName}>
+          <Grid item md={3} sm={5} xs={8}>
+            <Paper className={classes.paper}>{field.title}</Paper>
+          </Grid>
+          <Grid item md={1} sm={3} xs={4}>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => {
+                setModal({ open: true, ...field });
+              }}
+            >
+              <EditIcon />
+            </Button>
+            <Dialog open={modalOpen} onClose={closeModal} aria-labelledby="form-dialog">
+              <DialogTitle>{modalTitle}</DialogTitle>
+              <DialogContent>
+                <DialogContentText>{modalDialogText}</DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id={modalStateName}
+                  label={modalStateName}
+                  type={modalTextType}
+                  fullWidth
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={closeModal} color="primary">
+                  Annulez
+                </Button>
+                <Button onClick={closeModal} color="primary">
+                  Enregistrer
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </Grid>
         </Grid>
-        <Grid item md={1} sm={3} xs={4}>
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              setOpenFirstname(true);
-            }}
-          >
-            <EditIcon />
-          </Button>
-          <Dialog
-            open={openFirstname}
-            onClose={() => {
-              setOpenFirstname(false);
-            }}
-            aria-labelledby="form-dialog-prenom"
-          >
-            <DialogTitle id="form-dialog-prenom">Prénom</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Veuillez entrer votre prénom</DialogContentText>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="firstname"
-                label="Prénom"
-                type="firstname"
-                fullWidth
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button
-                onClick={() => {
-                  setOpenFirstname(false);
-                }}
-                color="primary"
-              >
-                Annulez
-              </Button>
-              <Button
-                onClick={() => {
-                  setOpenFirstname(false);
-                }}
-                color="primary"
-              >
-                Enregistrer
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Grid>
-      </Grid>
-
-      <Grid container spacing={3}>
-        <Grid item md={3} sm={5} xs={8}>
-          <Paper className={classes.paper}>Nom</Paper>
-        </Grid>
-        <Grid item md={1} sm={3} xs={4}>
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              setOpenName(true);
-            }}
-          >
-            <EditIcon />
-          </Button>
-          <Dialog
-            open={openName}
-            onClose={() => {
-              setOpenName(false);
-            }}
-            aria-labelledby="form-dialog-nom"
-          >
-            <DialogTitle id="form-dialog-nom">Nom</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Veuillez entrer votre nom</DialogContentText>
-              <TextField autoFocus margin="dense" id="name" label="Nom" type="Name" fullWidth />
-            </DialogContent>
-            <DialogActions>
-              <Button
-                onClick={() => {
-                  setOpenName(false);
-                }}
-                color="primary"
-              >
-                Annulez
-              </Button>
-              <Button
-                onClick={() => {
-                  setOpenName(false);
-                }}
-                color="primary"
-              >
-                Enregistrer
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid item md={3} sm={5} xs={8}>
-          <Paper className={classes.paper}>Email</Paper>
-        </Grid>
-        <Grid item md={1} sm={3} xs={4}>
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              setOpenMail(true);
-            }}
-          >
-            <EditIcon />
-          </Button>
-          <Dialog
-            open={openMail}
-            onClose={() => {
-              setOpenMail(false);
-            }}
-            aria-labelledby="form-dialog-mail"
-          >
-            <DialogTitle id="form-dialog-mail">Email</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Veuillez entrer votre adresse mail</DialogContentText>
-              <TextField autoFocus margin="dense" id="mail" label="Email" type="mail" fullWidth />
-            </DialogContent>
-            <DialogActions>
-              <Button
-                onClick={() => {
-                  setOpenMail(false);
-                }}
-                color="primary"
-              >
-                Annulez
-              </Button>
-              <Button
-                onClick={() => {
-                  setOpenMail(false);
-                }}
-                color="primary"
-              >
-                Enregistrer
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid item md={3} sm={5} xs={8}>
-          <Paper className={classes.paper}>Mot de passe</Paper>
-        </Grid>
-        <Grid item md={1} sm={3} xs={4}>
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              setOpenPwd(true);
-            }}
-          >
-            <EditIcon />
-          </Button>
-          <Dialog
-            open={openPwd}
-            onClose={() => {
-              setOpenPwd(false);
-            }}
-            aria-labelledby="form-dialog-title"
-          >
-            <DialogTitle id="form-dialog-title">Mot de passe</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Veuillez entrer votre nouveau mot de passe</DialogContentText>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="password"
-                label="Mot de passe"
-                type="password"
-                fullWidth
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button
-                onClick={() => {
-                  setOpenPwd(false);
-                }}
-                color="primary"
-              >
-                Annulez
-              </Button>
-              <Button
-                onClick={() => {
-                  setOpenPwd(false);
-                }}
-                color="primary"
-              >
-                Enregistrer
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Grid>
-      </Grid>
-    </div>
+      ))}
+    </Fragment>
   );
 }
