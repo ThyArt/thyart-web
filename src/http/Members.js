495e9b1509f9d7eb73a6c1a4474f8d8ef9bbd340
Maxence Tisserant <maxence.tisserant@epitech.eu>
2019-11-16 23:26:42 +0100

    create member and update role of members

--- src/http/Members.js
+++ src/http/Members.js
@@ -13,12 +13,11 @@ export const GetCurrentMembers = () =>
     }
   });
 
-/*
-export const UpdateNewData = () =>
+const UpdateRoleHook = () =>
   useAxios(
     {
-      url: 'api/user',
-      method: methods.Patch,
+      url: 'api/user/role/',
+      method: methods.Post,
       headers: {
         Authorization: `Bearer ${cookie.get('accessToken').access_token}`
       }
@@ -27,4 +26,56 @@ export const UpdateNewData = () =>
       manual: true
     }
   );
-*/
+
+const UpdateRoleExecute = (
+  func,
+  id,
+  role
+) =>
+  func({
+    url: 'api/user/role/' + id,
+    data: {
+      role: role
+    }
+  });
+
+export const UpdateRole = {
+  hook: UpdateRoleHook,
+  execute: UpdateRoleExecute
+};
+
+const CreateMemberHook = () =>
+  useAxios(
+    {
+      url: 'api/user/member',
+      method: methods.Post,
+      headers: {
+        Authorization: `Bearer ${cookie.get('accessToken').access_token}`
+      }
+    },
+    {
+      manual: true
+    }
+  );
+
+const CreateMemberExecute = (
+  func,
+  email,
+  firstname,
+  lastname,
+  password,
+  name
+) => func({
+  data: {
+    firstname,
+    lastname,
+    name,
+    email,
+    password
+  }
+});
+
+export const CreateMember = {
+  hook: CreateMemberHook,
+  execute: CreateMemberExecute
+};
--- src/views/Members.jsx
+++ src/views/Members.jsx
@@ -1,44 +1,82 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import Table from 'components/Table/Table';
+import { each, map } from 'lodash';
 import Switch from '@material-ui/core/Switch';
-import { GetCurrentMembers } from 'http/Members';
-import { each } from 'lodash';
+import Snackbar from '@material-ui/core/Snackbar';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from 'components/Form/TextField';
+import Table from 'components/Table/Table';
+import Button from "components/CustomButtons/Button";
+import SnackBarWrapper from 'components/SnackBarWrapper/SnackBarWrapper';
+import GridContainer from 'components/Grid/GridContainer';
+import GridItem from 'components/Grid/GridItem';
+import { GetCurrentMembers, UpdateRole, CreateMember } from 'http/Members';
 
 export default function Members() {
-  const [{ data: getData, loading: getLoading }] = GetCurrentMembers();
+  const [{ data: currentData, loading }] = GetCurrentMembers();
+  const [{ error: errorUpdate }, executeUpdate] = UpdateRole.hook();
+  const [{ error: errorCreate }, executeCreate] = CreateMember.hook();
   const [myData, setMyData] = useState([]);
   const [upToDate, setUpToDate] = useState(false);
   const [key, setKey] = useState(Math.random());
+  const [snackbar, setSnackbar] = useState({ open: false, closedByButton: false });
+  const [openModal, setOpenModal] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [userFirstName, setUserFirstName] = useState("");
+  const [userLastName, setUserLastName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
   const header = ["Nom d'utilisateur", "Prénom", "Nom de Famille", "Adresse mail", "Permissions des membres"];
+  const modalFields = [
+    {content: "Nom d'utilisateur du nouveau membre", label: "Nom d'utilisateur", type: "text", var: userName, handle: setUserName},
+    {content: "Nom de famille du nouveau membre", label: "Nom de famille", type: "text", var: userLastName, handle: setUserLastName},
+    {content: "Prénom du nouveau membre", label: "Prénom", type: "text", var: userFirstName, handle: setUserFirstName},
+    {content: "Email du nouveau membre", label: "Email", type: "email", var: userEmail, handle: setUserEmail},
+    {content: "Mot de passe du nouveau membre", label: "Mot de passe", type: "password", var: userPassword, handle: setUserPassword},
+  ];
+
+  if ((errorUpdate || errorCreate) && !snackbar.closedByButton && !snackbar.open)
+    setSnackbar({ open: true, closedByButton: false });
 
   const roleSwitch = (id, role) => {
-    const isAdmin = (role === 'admin');
+    if (role === 'admin') return (<Fragment>(C'est vous !)</Fragment>);
+    const isAdmin = (role === 'admin' || role === 'gallerist');
     return (
       <Fragment>
         Membre
-        <Switch id={"checkbox#" + id} checked={isAdmin} onChange={() => changeMemberRole(id)} color="primary"/>
+        <Switch id={"checkbox#" + id} checked={isAdmin} onChange={() => changeMemberRole(id, isAdmin)} color="primary"/>
         Galeriste
       </Fragment>
     );
   };
 
-  const changeMemberRole = id => {
-    // API call to change member role with id
-    console.log('patch ' + id);
+  const changeMemberRole = (id, isAdmin) => {
+    const role = (isAdmin ? 'member' : 'gallerist');
+    UpdateRole.execute(executeUpdate, id, role);
     setUpToDate(false);
   };
 
-  const deleteMember = id => {
-    // API call to delete member with id
-    console.log('delete ' + id);
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbar({ open: false, closedByButton: true });
+  };
+
+  const createMember = () => {
+    CreateMember.execute(executeCreate, userEmail, userFirstName, userLastName, userPassword, userName);
+    setOpenModal(false);
     setUpToDate(false);
   };
 
   useEffect(() => {
-    const formatResult = async () => {
+    const formatResult = () => {
       let tmp = [];
-      each(getData['data'], obj => {
+      each(currentData['data'], obj => {
         tmp.push({
           id: obj['id'],
           username: obj['name'],
@@ -53,14 +91,77 @@ export default function Members() {
       setKey(Math.random());
     };
 
-    if (getData && !upToDate) {
-      formatResult().then(() => console.log(myData, upToDate));
+    if (currentData && !upToDate) {
+      formatResult();
     }
-  }, [upToDate, getData, myData]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [upToDate, currentData, myData]);
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setUserPassword("");
+    setUserEmail("");
+    setUserFirstName("");
+    setUserLastName("");
+    setUserName("");
+  };
 
   return (
     <Fragment>
-      <Table header={header} rows={myData} onDeleteClick={deleteMember} key={key}/>
+      <Button type="button" color="primary" onClick={() => setOpenModal(true)}>
+        Créer un membre
+      </Button>
+
+      <Table header={header} rows={myData} key={key}/>
+      
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogTitle>Créer un nouveau membre</DialogTitle>
+        <DialogContent>
+          <GridContainer spacing={3}>
+            { map(modalFields, (fields, it) => { return (
+              <Fragment key={'field#' + it}>
+                <GridItem xs={6}>
+                  <DialogContentText style={{marginTop: '10%'}}>
+                    {fields['content']}
+                  </DialogContentText>
+                </GridItem>
+                <GridItem xs={6}>
+                  <TextField
+                    label={fields['label']}
+                    type={fields['type']}
+                    value={fields['var']}
+                    onChange={event => fields['handle'](event.target.value)}
+                  />
+                </GridItem>
+              </Fragment>
+            );})}
+          </GridContainer>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal} color="transparent">
+            Annuler
+          </Button>
+          <Button onClick={createMember} color="primary">
+            Valider
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right'
+        }}
+        open={snackbar.open}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <SnackBarWrapper
+          variant="error"
+          message="Erreur, l'action n'a pas pu être exécutée."
+          onClose={handleCloseSnackbar}
+        />
+      </Snackbar>
     </Fragment>
   );
 };
