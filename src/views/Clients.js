3b00eb9947087f7dc5e48b10cdff585de9a4ca07
Pierre <miganipierre@live.fr>
2019-11-16 16:27:36 +0100

    added delete client

--- src/views/Clients.js
+++ src/views/Clients.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { GetCustomers as CustomerRequest } from 'http/Customer';
+import { DeleteCustomer } from 'http/Customer';
 import GridContainer from 'components/Grid/GridContainer';
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
@@ -16,41 +17,50 @@ import Cookies from 'universal-cookie';
 export default function Clients() {
     const cookie = new Cookies();
     var token = cookie.get('accessToken');
-    var [{ data, error }, execute] = CustomerRequest.hook();
     const [table, setTable] = useState(true);
     const [isNew, setIsNew] = useState(true);
     const [clients, setClients] = useState([]);
     const [selected, setSelected] = useState(-1);
-    const [rowsName, setRowsName] = useState([]);
-    const [rowsKey, setRowsKey] = useState([]);
-    const [receivedData, setReceivedData] = useState(false);
+    const [rowsName, setRowsName] = useState([
+        'Email',
+        'Prénom',
+        'Nom de famille'
+    ]);
+    const [rowsKey, setRowsKey] = useState([
+        'email',
+        'first_name',
+        'last_name'
+    ]);
     const [key, setKey] = useState(Math.random());
+    var [{ data, loading, error }, refresh] = CustomerRequest(token.access_token);
+    var dataRequest = data;
+    var [{ response, error }, execute] = DeleteCustomer.hook(token.access_token);
+    var responseDelete = response;
 
-    // console.log(token)
     var content;
-
-    useEffect(() => {
-        setRowsName([
-            'Email',
-            'Prénom',
-            'Nom de famille'
-        ]);
-        setRowsKey([
-            'email',
-            'first_name',
-            'last_name'
-        ]);
-
-            CustomerRequest.execute(execute, token.access_token);
-    }, []);
     
     useEffect(() => {
-        if (data)
-            setClients(data.data);
-        setKey(Math.random());
-    }, [data]);
+        refresh(token.access_token);
+    }, []);
 
-    if (table) {
+    useEffect(() => {
+
+        if (dataRequest)
+        {
+            console.log(responseDelete);
+            setClients(dataRequest.data);
+        }
+        setKey(Math.random());
+    }, [dataRequest]);
+    
+    useEffect(() => {
+        if (responseDelete)
+        {
+            refresh(token.access_token);
+        }
+    }, [responseDelete]);
+    
+    if (table && loading === false) {
         content = <div>
             <Button type="button" color="primary" onClick={() => {
                 setTable(false);
@@ -60,21 +70,25 @@ export default function Clients() {
             >
                 Créer un client
             </Button>
-            <Table header={rowsName} rows={clients} key={key} keys={rowsKey} onRowClick={(id) => {
+            <Table header={rowsName} rows={clients} key={key} keys={rowsKey} onDeleteClick={(id) => {
+                DeleteCustomer.execute(execute, id);
+            }}
+            onRowClick={(id) => {
                 setSelected(id);
                 setIsNew(false);
                 setTable(false);    
             }}/>
         </div>
     }
-    else
+    else if (loading === false)
         content = <ClientDetails isNew={isNew} clientId={selected} returnFunction={() => {
-                CustomerRequest.execute(execute, token.access_token);
+                refresh(token.access_token);
                 setTable(true);
-                setReceivedData(false);
                 }
             } 
         />
+    else
+        content = <div></div>
 
     return (
         <div>
