4bd9d5fa5257935efb49497008e9f90b7cfc75a4
Clément <clement.deseine@gmail.com>
2019-11-17 12:24:12 +0100

    added Artwork page

--- src/routesDashboard.js
+++ src/routesDashboard.js
@@ -1,18 +1,20 @@
 // @material-ui/icons
+import Event from '@material-ui/icons/Event';
 import Person from '@material-ui/icons/Person';
 import GroupIcon from '@material-ui/icons/Group';
+import Brush from '@material-ui/icons/Brush';
+
 import UserProfile from 'views/UserProfile';
 import Clients from 'views/Clients';
 import Calendar from 'views/Calendar';
 import Members from 'views/Members';
-// core components/views for Admin layout
-// core components/views for RTL layout
+import Artworks from 'views/Artworks';
 
 const dashboardRoutes = [
   {
     path: '/accueil',
     name: 'Accueil',
-    icon: Person,
+    icon: Event,
     component: Calendar,
     layout: '/dashboard'
   },
@@ -35,6 +37,13 @@ const dashboardRoutes = [
     name: 'Clients',
     icon: Person,
     component: Clients,
+    layout: '/dashboard'
+  },
+  {
+    path: '/artworks',
+    name: 'Oeuvre d\'arts',
+    icon: Brush,
+    component: Artworks,
     layout: '/dashboard'
   }
 ];
