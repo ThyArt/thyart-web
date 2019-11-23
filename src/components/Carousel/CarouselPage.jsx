a15063b9ac25727b74cdb0a091c810b076e6e507
Jean <jean-gabriel.greco@epitech.eu>
2019-11-23 16:40:15 +0100

    add images in carousel //todo ajouter texte sur image

--- src/components/Carousel/CarouselPage.jsx
+++ src/components/Carousel/CarouselPage.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
+import carImg1 from 'assets/img/carousel.png';
+import carImg2 from 'assets/img/galerie.png';
+import carImg3 from 'assets/img/mess.png';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+const useStyles = makeStyles({
+  container: {
+    height: '70vh'
+  }
+});
 
 export default function CarouselPage() {
+  const classes = useStyles();
   return (
-    <AwesomeSlider>
-      <div data-src="/path/to/image-0.png" />
-      <div data-src="/path/to/image-1.png" />
-      <div data-src="/path/to/image-2.jpg" />
-    </AwesomeSlider>
+    <AutoplaySlider
+      play
+      cancelOnInteraction={false} // should stop playing on user interaction
+      interval={6000}
+      infinite
+      className={classes.container}
+    >
+      <div data-src={carImg1} />
+      <div data-src={carImg2} />
+      <div data-src={carImg3} />
+    </AutoplaySlider>
   );
 }
